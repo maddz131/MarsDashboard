61ff85557355deda0ceb0be8c35fce2d64fc9a4f
maddz131 <maddz131@gmail.com>
2022-04-16 19:26:48 -0700

    Added endpoint for retrieving photos from nasa. Made store an immutable object.

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,103 +1,75 @@
-
-let store = {
+let store = Immutable.Map({
     user: { name: "Student" },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
-
+    data: [],
+    images: []
+});
 // add our markup to the page
 const root = document.getElementById('root')
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+const updateStore = (state, newState) => {
+    store = state.merge(newState);
+    render(root, store);
+  };
 
-const render = async (root, state) => {
+const render = async(root, state) => {
     root.innerHTML = App(state)
 }
 
 
-const router = async () => {
-    console.log(location.pathname)
-    const routes = [
-        {path: "/", action: () => console.log("Viewing Dashboard")},
-        {path: "/Opportunity", action: () => {roverData(store, "/Opportunity"); console.log(this.path)}},
-        {path: "/Curiosity", action: () => {roverData(store, "/Curiosity"); console.log("Viewing Curiosity")}},
-        {path: "/Spirit", action: () => {roverData(store, "/Spirit"); console.log("Viewing Spirit")}}
-    ];
-
-    //Test each route for potential match
-    const potentialMatches = routes.map(route => {
-        return {
-            route: route,
-            isMatch: location.pathname === route.path
-        };
-    });
-
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
-
-    if(!match) {
-        match = {
-            route: routes[0],
-            isMatch: true
-        };
-    }
-    match.route.action();
-};
+const getRoverTabs = (rovers) => {
+    const roverTabs = [`<a href="/" class = "nav__link" data-link> Home </a>`]
+    roverTabs.push(rovers.map(x => {return `<a href="/${x}" class = "nav__link" data-link> ${x} </a>`}))
+    return roverTabs
+}
 
 // create content
 const App = (state) => {
-    let { apod } = state
-    let viewObject = [].concat(state)
-    console.log(viewObject)
+    let name = state.get("user").name
+    let apod = state.get("apod")
+    let rovers = state.get("rovers")
+    let latest_photos  = state.get("images").latest_photos
+    let photo_manifest = state.get("data").photo_manifest
+    let currentRover = state.get("path").slice(1)
 
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <body>
-                <nav class = "nav">
-                    <a href="/${store.rovers[0]}" class = "nav__link" data-link> ${store.rovers[0]} </a>
-                    <a href="/${store.rovers[1]}" class = "nav__link" data-link> ${store.rovers[1]} </a>
-                    <a href="/${store.rovers[2]}" class = "nav__link" data-link> ${store.rovers[2]} </a>
-                </nav>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>${Object.keys(state)}</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </body>
-        </main>
-        <footer></footer>
-    `
-}
+    console.log(state.entries())
 
-
-const navigateTo = url => {
-    history.pushState(null, null, url);
-    router();
+    if(state.get("path") != '/'){
+        return `<nav class = "nav">
+                        ${getRoverTabs(rovers)}
+                    </nav>
+                    <div id = 'app'>
+                        <h1>${Greeting(name)}</>
+                        <h3>Here you can learn more about the rover ${currentRover}</h3>
+                        ${roverInfoGraph(photo_manifest, latest_photos)}
+                    </div>`
+        }else{
+            return `<nav class = "nav">
+                        ${getRoverTabs(rovers)}
+                    </nav>
+                    <div id = 'app'>
+                        <h1>${Greeting(name)}</>
+                        <p>
+                            One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+                            the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+                            This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+                            applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+                            explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+                            but generally help with discoverability of relevant imagery.
+                        </p>
+                        ${ImageOfTheDay(apod, state)}
+                    </div>
+                <footer></footer>`
+        }
 }
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    console.log("here")
-    document.addEventListener("click", e => {
-        console.log("now here")
-        if(e.target.matches("data-link")){
-            e.preventDefault();
-            navigateTo(e.target.href)
-        }
-    })
+
+    //router()
     router()
-    render(root, store)
+    //render(root, store)
 })
 
 // ------------------------------------------------------  COMPONENTS
@@ -109,23 +81,21 @@ const Greeting = (name) => {
             <h1>Welcome, ${name}!</h1>
         `
     }
-
     return `
         <h1>Hello!</h1>
     `
 }
-
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+const ImageOfTheDay = (apod, state) => {
 
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+    //const photodate = new Date(apod.date)
+    //console.log(photodate.getDate(), today.getDate());
 
-    console.log(photodate.getDate() === today.getDate());
+    //console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
+        getImageOfTheDay(state)
     }
 
     // check if the photo of the day is actually type video!
@@ -141,6 +111,7 @@ const ImageOfTheDay = (apod) => {
             <p>${apod.image.explanation}</p>
         `)
     }
+
 }
 
 // ------------------------------------------------------  API CALLS
@@ -156,13 +127,81 @@ const getImageOfTheDay = (state) => {
     return apod
 }
 
-const roverData = (state, rover) => {
-    let { apod } = state
-    console.log("fetching data")
-    fetch(`http://localhost:3000${rover}Data`)
-        .then(res => res.json())
-        .then(json => updateStore(store, json))
+const router = () => {
 
-    //return data
+    //acceptable paths
+    const routes = [
+        {path: "/", action: () => console.log("Viewing Dashboard")},
+        {path: "/Opportunity", action: () => { console.log("Viewing Opportunity")}},
+        {path: "/Curiosity", action: () => {console.log("Viewing Curiosity")}},
+        {path: "/Spirit", action: () => {console.log("Viewing Spirit")}}
+    ];
+
+    //Test each route to see if it matches our current path location
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+
+    //if the path isn't one of our acceptable routes, set it to the default path
+    if(!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+    if(location.pathname != "/"){
+        getRoverData(match.route.path)
+        getRoverImages(match.route.path)
+    }
+    console.log(store)
+    console.log("adding path to store")
+    match.route.action
+    updateStore(store, match.route)
 }
 
+const getRoverData = (rover) => {
+    console.log("fetching data for " + rover)
+    if(rover != "/"){
+        fetch(`http://localhost:3000${rover}/data`)
+            .then(res => res.json())
+            .then(data => updateStore(store, data))
+            .then(()=>console.log("finished fetching data"))
+    }
+}
+
+const getRoverImages = (rover) => {
+    console.log("fetching images")
+    if(rover != "/"){
+        fetch(`http://localhost:3000${rover}/photos`)
+            .then(res => res.json())
+            .then(images => updateStore(store, images))
+            .then(()=>console.log("finished fetching images"))
+    }
+}
+
+const imageGrid = (imageList) => {
+    let imageSources = imageList.map(photo => `<img src=${photo.img_src}/>`)
+    return imageSources.reduce((acc, curr)=> {
+        return acc += curr;
+    })
+}
+
+const roverInfoGraph = ({ name, launch_date, landing_date, status, max_date }, latest_photos) => {
+    console.log("Made graph")
+    console.log(name)
+        return (
+            `<ul>
+                <li>Rover Name: ${name}</li>
+                <li>Launch Date: ${launch_date}</li>
+                <li>Landing Date: ${landing_date}</li>
+                <li>Mission Status: ${status}</li>
+                <li>Latest Photos: ${max_date}</li>
+            </ul>
+            <p>${imageGrid(latest_photos)}</p>`
+        )
+}
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -11,10 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get(/\/*(.js)$/, express.static(path.join(__dirname, '../public')))
+app.get(/\/*(.css)$/, express.static(path.join(__dirname, '../public')))
 
 app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`,  { 
+            mode: 'no-cors' // 'cors' by default
+        })
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
@@ -23,7 +26,7 @@ app.get('/apod', async (req, res) => {
 })
 
 // your API calls
-app.get(/\/*(Data)$/, async (req, res) => {
+app.get(/\/*\/(data)$/, async (req, res) => {
     let rover = req.url.slice(0, -4)
     try {
         let data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`)
@@ -34,9 +37,22 @@ app.get(/\/*(Data)$/, async (req, res) => {
     }
 })
 
+
+app.get(/\/*\/(photos)$/, async (req, res) => {
+    let rover = req.url.slice(0, -6)
+    //let date = req.
+    try {
+        let images = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ images })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+
 app.get('/*', async (req, res) => {
     res.sendFile(path.resolve(__dirname, "../public", "index.html"))
 })
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
